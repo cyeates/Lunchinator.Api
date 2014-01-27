7cfcb2b62809e7c3d5c6d47c19d0aeda194c8f9e
Chad Yeates <chad.yeates@gmail.com>
2014-01-27 11:46:14 -0600

    injecting dependencies

--- Lunchinator.Api/App_Start/UnityConfig.cs
+++ Lunchinator.Api/App_Start/UnityConfig.cs
@@ -1,21 +1,34 @@
+using Lunchinator.Data;
+using Lunchinator.Domain;
+using Lunchinator.Domain.BusinessApi;
+using Lunchinator.Domain.Services;
 using Microsoft.Practices.Unity;
 using System.Web.Http;
 using Unity.WebApi;
 
 namespace Lunchinator.Api
 {
-    public static class UnityConfig
+  public static class UnityConfig
+  {
+    public static void RegisterComponents()
     {
-        public static void RegisterComponents()
-        {
-			var container = new UnityContainer();
-            
-            // register all your components with the container here
-            // it is NOT necessary to register your controllers
-            
-            // e.g. container.RegisterType<ITestService, TestService>();
-            
-            GlobalConfiguration.Configuration.DependencyResolver = new UnityDependencyResolver(container);
-        }
+      var container = new UnityContainer();
+
+      container.RegisterType<LunchinatorContext, LunchinatorContext>();
+
+      //recommendation engine
+      container.RegisterType<IGroupRecommendationAlgorithm, AverageOfRatings>();
+      container.RegisterType<ISimilarityAlgorithm, PearsonCorrelation>();
+      container.RegisterType<Recommendation, Recommendation>();
+
+      //service
+      container.RegisterType<LunchService, LunchService>();
+      container.RegisterType<UserService, UserService>();
+
+      //restaurant api
+      container.RegisterType<IBusinessApiProvider, YelpProvider>();
+
+      GlobalConfiguration.Configuration.DependencyResolver = new UnityDependencyResolver(container);
     }
+  }
 }
